732f081780a78f99da54e12a0fb5c954a7f6eb10
SaiVinay072 <yallasaivinay1@gmail.com>
2021-12-01 13:36:59 +0530

    Intial commit

---
+++ src/main/java/com/Sasken/spring/mockito/api/dao/UserRepository.java
@@ -0,0 +1,14 @@
+package com.Sasken.spring.mockito.api.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Sasken.spring.mockito.api.model.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+	
+	List<User> findByAddress(String address);
+
+
+}
